f94814a9da410ad7d1e21c69f9513108038076f1
unknown <faizanmohammed746@gmail.com>
2024-12-04 10:02:39 +0530

    first commit

---
+++ src/Layout/Navbar/Navbar.jsx
@@ -0,0 +1,175 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Container } from "@mui/material";
+import logo from "../../Assets/Images/Logo.png";
+
+const drawerWidth = 240;
+const navItems = [
+  "today special offer",
+  "why food hut",
+  "our menu",
+  "our popular food",
+];
+
+export default function Navbar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        <img src={logo} alt="Logo" />
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        backgroundColor: "transparent",
+        position: "absolute",
+        top: "0",
+        left: "0",
+        height: "auto",
+        padding: "0px!important",
+        justifyContent:"space-between"
+      }}
+    >
+      <CssBaseline />
+      <AppBar
+        component="nav"
+        sx={{
+          position: "realtive",
+          backgroundColor: "transparent",
+          color: "#fff",
+          boxShadow: "none",
+          top: "0",
+          left: "0",
+          padding: "20px 0px",
+        }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="div"
+              sx={{
+                flexGrow: 1,
+                display: {
+                  xs: "none",
+                  sm: "block",
+                  padding: "0px",
+                  width: "121px",
+                  height: "78px",
+                },
+              }}
+            >
+              <img
+                src={logo}
+                alt="Logo"
+                style={{ width: "121px", height: "78px" }}
+              />
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              {navItems.map((item) => (
+                <Button
+                  key={item}
+                  sx={{
+                    color: "#fff",
+                    textTransform: "capitalize",
+                    fontFamily: "Poppins",
+                    fontWeight: "Regular",
+                    fontSize: {lg:"16px",md:"14px",sm:"11px"},
+                    padding: {lg:"20px",md:"10px",sm:"5px"},
+                  }}
+                >
+                  {item}
+                </Button>
+              ))}
+            </Box>
+            <Typography sx={{ paddingLeft: {md:"55px",xs:"0px",lg:"90px",sm:"20px"},marginLeft:"auto" }}>
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "#F65F5F",
+                  borderRadius: "100px",
+                  fontFamily: "Poppins",
+                  textTransform: "capitalize",
+                  color:"#000",
+                  fontSize:{md:"16px",sm:"14px",md:"14px"},
+                  fontWeight:"Regular",
+                  boxShadow:"none",
+                  padding:{xs:"5px 15px!important",md:"8px 15px!important"}
+                }}
+              >
+                Download App
+              </Button>
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+      </Box>
+    </Box>
+  );
+}
---
+++ src/cms/Home/Home.jsx
@@ -0,0 +1,201 @@
+import React from "react";
+import Hero from "../Home/Home.module.css";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Navbar from "../../Layout/Navbar/Navbar";
+import linear from "../../Assets/Images/Group 8438.png";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import SearchIcon from "@mui/icons-material/Search";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import herosecimage from "../../Assets/Images/Bannersecgirl.png";
+
+const Home = () => {
+  return (
+    <>
+      <Box className={Hero.Background}>
+        <Navbar />
+        {/* linearpattern box */}
+        <Box className={Hero.linearStyle}>
+          <img src={linear} alt="" />
+        </Box>
+        {/* end of linear pattern box */}
+        {/* start banner section */}
+        <Container maxWidth="xl">
+          <Grid container>
+            <Grid
+              sm={12}
+              md={6}
+              sx={{ paddingLeft: { md: "20px", xs: "10px" } }}
+            >
+              <Box
+                sx={{
+                  paddingTop: { sm: "70px", xs: "30px" },
+                  paddingBottom: "30px",
+                }}
+              >
+                <Button
+                  variant="contained"
+                  sx={{
+                    color: "#fff",
+                    textTransform: "capitalize",
+                    fontFamily: "Inter",
+                    fontSize: "14px",
+                    fontWeight: "Regular",
+                    borderRadius: "100px",
+                    backgroundColor: "rgba(246, 95, 95, 0.4)",
+                    padding: "5px 15px",
+                    paddingLeft: "7px",
+                  }}
+                >
+                  <FavoriteIcon
+                    sx={{
+                      marginRight: "5px",
+                      color: "#FDCE77",
+                      backgroundColor: "#F65F5F",
+                      borderRadius: "50%",
+                      padding: "3px",
+                    }}
+                  />{" "}
+                  People Trust us
+                </Button>
+              </Box>
+              <Typography
+                className={Hero.heroSecHeading}
+                sx={{
+                  fontSize: {
+                    xs: "48px!important",
+                    lineHeight: "135%",
+                    fontWeight: "600!important",
+                    letterSpacing: "0%",
+                  },
+                }}
+              >
+                We're <span style={{ color: "#F54748" }}> Serious </span>{" "}
+                <br className={Hero.break} /> For{" "}
+                <span style={{ color: "#F54748" }}>Food</span> &{" "}
+                <span style={{ color: "#FDC55E" }}>Delivery.</span>
+              </Typography>
+              <Typography
+                className={Hero.bannerSecPara}
+                sx={{ xs: { fontSize: "20px!important" } }}
+              >
+                Best cooks and best delivery guys all at your{" "}
+                <br className={Hero.breakk} /> service. Hot tasty food will
+                reach you in 60 <br className={Hero.breakk} /> minutes.
+              </Typography>
+              <Box sx={{ paddingBottom: "40px" }}>
+                <TextField
+                  placeholder="Search Food"
+                  sx={{
+                    backgroundColor: "#0D0D0D",
+                    border: "1px solid #fff",
+                    width: { sm: "70%", xs: "100%", md: "70%" },
+                    borderRadius: "100px",
+                    "& .MuiInputBase-input": {
+                      color: "white", // Text color
+                      fontFamily: "Poppins!important",
+                      fontSize: "18px",
+                      fontWeight: "Regular",
+                      outline: "none",
+                    },
+                    paddingLeft: "20px",
+                  }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SearchIcon sx={{ color: "#fff" }} />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Box>
+              <Box sx={{ display: "flex" }}>
+                <Button
+                  variant="contained"
+                  sx={{
+                    backgroundColor: "#F65F5F",
+                    borderRadius: "100px",
+                    fontFamily: "Poppins",
+                    textTransform: "capitalize",
+                    color: "#000",
+                    fontSize: { md: "16px", sm: "14px", md: "14px" },
+                    fontWeight: "Regular",
+                    boxShadow: "none",
+                    padding: {
+                      xs: "5px 15px!important",
+                      md: "8px 15px!important",
+                    },
+                    marginRight: "20px",
+                  }}
+                >
+                  Download App
+                </Button>
+                <Button
+                  sx={{
+                    borderRadius: "100px",
+                    fontFamily: "Poppins",
+                    textTransform: "capitalize",
+                    color: "#fff",
+                    fontSize: { md: "16px", sm: "14px", md: "14px" },
+                    fontWeight: "Regular",
+                    boxShadow: "none",
+                    padding: {
+                      xs: "5px 15px!important",
+                      md: "8px 15px!important",
+                    },
+                  }}
+                >
+                  <PlayArrowIcon
+                    sx={{
+                      marginRight: "8px",
+                      color: "#F65F5F",
+                      backgroundColor: "#0D0D0D",
+                      boxShadow: "0px 10px 30px #DF6951",
+                      borderRadius: "50%",
+                      width: "22px",
+                      height: "22px",
+                    }}
+                  />{" "}
+                  Watch Video
+                </Button>
+              </Box>
+            </Grid>
+            <Grid sm={12} md={6}>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: "70px 0px",
+                }}
+              >
+                <Box className={Hero.leftside}>
+                  <img
+                    src={herosecimage}
+                    alt=""
+                    style={{
+                      position: "absolute",
+                      bottom: "15px",
+                      zIndex: "99",
+                      
+                    }}
+                    className={Hero.bannergirlimage}
+                  />
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default Home;
